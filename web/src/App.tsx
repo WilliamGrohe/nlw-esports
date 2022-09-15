02bd8f9f6f26712a5adda932b91808dd73553c7b
William Grohe <william.grohe@gmail.com>
2022-09-14 22:33:11 -0300

    criado landing page estática web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
 import "./styles/main.css";
 
 import logoImg from "./assets/logo-nlw-esports.svg";
@@ -8,11 +9,7 @@ function App() {
       <img src={logoImg} alt="" />
 
       <h1 className="text-6xl text-white font-black mt-20">
-        Seu
-        <span className="bg-nlw-gradient text-transparent bg-clip-text">
-          duo
-        </span>{" "}
-        está aqui.
+        Seu <span className="bg-nlw-gradient text-transparent bg-clip-text">duo</span> está aqui.
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
@@ -82,6 +79,21 @@ function App() {
           </div>
         </a>
       </div>
+
+      <div className="bg-nlw-gradient pt-1 self-stretch mt-8 rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] py-6 px-8 justify-between items-center flex">
+          <div className="">
+            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
+
     </div>
   );
 }
